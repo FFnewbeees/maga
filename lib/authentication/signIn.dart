71b2f6dc936b9fd04d74118f8fcdaaa4b9f914f5
FFnewbeees <victorliu1999@gmail.com>
2019-11-21 09:25:03 +1100

    authentication done

--- lib/authentication/signIn.dart
+++ lib/authentication/signIn.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:maga/authentication/signUp.dart';
 import '../pages/home.dart';
 
 class SignInPage extends StatefulWidget{
@@ -23,33 +24,67 @@ class _SignInPageState extends State<SignInPage>{
         key: _formKey,
         child: Column(
           children: <Widget>[
-            TextFormField(
-              validator: (input){
-                if(input.isEmpty){
-                  return 'Please type an email';
-                }
-              },
-              onSaved:(input) => _email = input ,
-              decoration: InputDecoration(
-                labelText: 'Email:'
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+              child: TextFormField(
+                validator: (input){
+                  if(input.isEmpty){
+                    return 'Please type an email';
+                  }
+                },
+                onSaved:(input) => _email = input ,
+                decoration: InputDecoration(
+                  labelText: 'Email:'
+                ),
               ),
             ),
-            TextFormField(
-              validator: (input){
-                if(input.length < 6){
-                  return 'Your password needs to be at least 6 characters';
-                }
-              },
-              onSaved:(input) => _password = input ,
-              decoration: InputDecoration(
-                labelText: 'Password:'
-              ),
-              obscureText: true,
+            SizedBox(
+              height: 15.0,
             ),
-            RaisedButton(
-              onPressed: signIn,
-              child: Text('Sign In'),
-            )
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+              child: TextFormField(
+                validator: (input){
+                  if(input.length < 6){
+                    return 'Your password needs to be at least 6 characters';
+                  }
+                },
+                onSaved:(input) => _password = input ,
+                decoration: InputDecoration(
+                  labelText: 'Password:'
+                ),
+                obscureText: true,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 20.0, right: 20.0, top: 10.0),
+                    child: GestureDetector(
+                      onTap: signIn,
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 60.0,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF18D191),
+                          borderRadius: BorderRadius.circular(9.0)),
+                        child: Text('Sign In',
+                          style: TextStyle(
+                            color: Colors.white, fontSize: 20.0),
+                            ),
+                        ),
+                    ),
+                ),
+              )
+            ],
+          ),
+          Expanded(child: Column(children: <Widget>[],),)
           ],
         ),
       ),
@@ -60,8 +95,10 @@ void signIn() async{
     final formState = _formKey.currentState;
     if(formState.validate()){
       formState.save();
+      print("fk1");
       try{
-          FirebaseUser user = (await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password)) as FirebaseUser;
+          AuthResult result = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
+          FirebaseUser user = result.user;
           Navigator.pushReplacement(
             context,
             MaterialPageRoute(builder: (context) => Home(user:user)));
--- lib/authentication/signUp.dart
+++ lib/authentication/signUp.dart
@@ -23,33 +23,63 @@ class _SignUpPageState extends State<SignUpPage> {
         key: _formKey,
         child: Column(
           children: <Widget>[
-            TextFormField(
-              validator: (input){
-                if(input.isEmpty){
-                  return('Provide an email');
-                }
-              },
-              decoration: InputDecoration(
-                labelText: 'Email'
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+              child: TextFormField(
+                validator: (input){
+                  if(input.isEmpty){
+                    return('Provide an email');
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: 'Email'
+                ),
+                onSaved: (input) => _email = input,
               ),
-              onSaved: (input) => _email = input,
             ),
-            TextFormField(
-              validator: (input){
-                if(input.length < 6){
-                  return 'Longer password please';
-                }
-              },
-              decoration: InputDecoration(
-                labelText: 'Password'
+            SizedBox(
+              height: 15.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+              child: TextFormField(
+                validator: (input){
+                  if(input.length < 6){
+                    return 'Longer password please';
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: 'Password'
+                ),
+                onSaved: (input) => _password = input,
+                obscureText: true,
               ),
-              onSaved: (input) => _password = input,
-              obscureText: true,
             ),
-            RaisedButton(
-              onPressed: signUp,
-              child: Text('Sign Up'),
-            )
+                        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 20.0, right: 20.0, top: 10.0),
+                    child: GestureDetector(
+                      onTap: signUp,
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 60.0,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF4364A1),
+                          borderRadius: BorderRadius.circular(9.0)),
+                        child: Text('Sign Up',
+                          style: TextStyle(
+                            color: Colors.white, fontSize: 20.0),
+                            ),
+                        ),
+                    ),
+                ),
+              )
+            ],
+          ),
           ],
         ),
       ),
