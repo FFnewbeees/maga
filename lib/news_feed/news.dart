5752a87df0e21c199a1004906468b0d330906498
FFnewbeees <victorliu1999@gmail.com>
2019-11-21 12:00:33 +1100

    swap fb connection

---
+++ lib/news_feed/news.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+class NewsPage extends StatefulWidget {
+  @override
+  _NewsPageState createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsPage> {
+
+  //styling const
+  final Color bgColor = Color(0xffF3F3F3);
+  final Color primaryColor = Color(0xffE70F0B);
+
+  var titleTextStyle = TextStyle(
+    color: Colors.black87,
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+    ); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/ui_design/loginUI.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Container(
+        padding: const EdgeInsets.all(16.0),
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.lightGreen,
+              Colors.green
+            ]
+          )
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 40.0, bottom: 20.0),
+              height: 80,
+
+            )
+        ],),
+      )
+      
+    );
+  }
+}
