9e24520a81ce6faa5d96bc44fbe748280f20bd63
99032000 <z3439119@gmail.com>
2020-01-16 11:56:06 +1100

    Camera and Gallery

--- lib/camera/camera_screen.dart
+++ lib/camera/camera_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:maga/camera/pictureItem.dart';
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -8,8 +11,59 @@ class CameraScreen extends StatefulWidget {
 class _CameraScreenState extends State<CameraScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Stack(
+      children: <Widget>[
+        ListView.builder(
+          itemBuilder: (ctx, index) {
+            return PictureItem();
+          },
+          itemCount: 1,
+        ),
+        Positioned(
+          bottom: 20,
+          right: 10,
+          child: FloatingActionButton(
+            child: Icon(Icons.camera_alt),
+            onPressed: _cameraAction,
+          ),
+        ),
+      ],
     );
+  }
+
+  void _cameraAction() {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (ctx) {
+        return new CupertinoActionSheet(
+          actions: <Widget>[
+            CupertinoActionSheetAction(
+              child: const Text('Open Camera'),
+              onPressed: (){
+                _getImage(context, ImageSource.camera);
+                Navigator.of(context).pop();
+              },
+            ),
+            CupertinoActionSheetAction(
+              child: const Text('Open Gallery'),
+              onPressed: (){
+                _getImage(context, ImageSource.gallery);
+                Navigator.of(context).pop();
+              },
+            ),
+            SizedBox(height: 20,),
+            CupertinoActionSheetAction(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      }
+    );
+  }
+  void _getImage(BuildContext context, ImageSource imageSource){  
+    ImagePicker.pickImage(source: imageSource);
   }
 }
---
+++ lib/camera/pictureItem.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class PictureItem extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: ()=> null,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 4,
+        margin: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  child: Image.asset(
+                    'assets/recycling.jpg',
+                    height: 200,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.date_range,
+                      ),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('18/01/2020'),
+                    ],
+                  ),
+                 
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.done_outline,
+                      ),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('recycle'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
