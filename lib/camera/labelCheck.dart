0f4ac318c77ee56bd4f64f7f29dbeea7a9c6dc0b
99032000 <z3439119@gmail.com>
2020-01-29 16:29:44 +1100

    firebase coding

---
+++ lib/camera/labelCheck.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:maga/camera/recycleModel.dart';
+
+class LabelCheck {
+  //final String label;
+  //LabelCheck(this.label);
+  // 0 is not recycle 1 is recycle 2 is booking for council collect.3 e-waste
+  int check(String label) {
+    String formatted = label.toLowerCase().trim();
+    if (formatted.contains('bottle')) {
+      return 1;
+    }
+    if (formatted.contains('furniture')) {
+      return 2;
+    }
+    if(formatted.contains('electronic')){
+      return 3;
+    }
+    if (formatted.contains('plastic')) {
+      if (formatted.compareTo('plastic wrap') == 0 ||
+          formatted.compareTo('plastic bag') == 0) return 0;
+      return 1;
+    }
+    if (formatted.contains('glass') && formatted.contains('bottle')) {
+      return 1;
+    }
+    if (formatted.contains('cardboard') || formatted.contains('carton')) {
+      return 1;
+    }
+    if (formatted.contains('metal') || formatted.contains('copper')) {
+      return 1;
+    }
+    return 0;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '././news_feed/newsDetail.dart';
 import './navigation/tabs.dart';
@@ -8,6 +9,7 @@ import './authentication/signIn.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
