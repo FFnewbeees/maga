aaffcbfbd428dc9b275402cde06aaf29d156237a
99032000 <z3439119@gmail.com>
2020-02-12 12:13:42 +1100

    new page bug fix

--- lib/news_feed/newsPage.dart
+++ lib/news_feed/newsPage.dart
@@ -6,116 +6,107 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:maga/loader/loader.dart';
 
-
 class NewsPage extends StatefulWidget {
   @override
   final FirebaseUser user;
   NewsPage(this.user);
   _NewsPageState createState() => _NewsPageState();
-  
 }
 
 class _NewsPageState extends State<NewsPage> {
-
-  List<News> _items= [];
+  List<News> _items = [];
   bool _isLoading = false;
   bool _isInit = true;
-  
-  Future<void> fetchNews() async{
 
-          _isLoading = true;
- 
-          const url = 'https://content.guardianapis.com/search?q=australia&tag=environment/recycling&from-date=2018-01-01&show-tags=contributor&show-fields=body,thumbnail,short-url&show-refinements=all&order-by=relevance&api-key=ed144467-d910-464d-93bd-f864c65a3b1c&page-size=50';
-           
-          try{
-           
-            
-            final response = await http.get(url);
+  Future<void> fetchNews() async {
+    _isLoading = true;
 
-            final data = json.decode(utf8.decode(response.bodyBytes));
-           
-            final articleData = data['response']['results'] as List<dynamic>;
+    const url =
+        'https://content.guardianapis.com/search?q=australia&tag=environment/recycling&from-date=2018-01-01&show-tags=contributor&show-fields=body,thumbnail,short-url&show-refinements=all&order-by=relevance&api-key=ed144467-d910-464d-93bd-f864c65a3b1c&page-size=50';
 
-            final List<News> loadedNews = [];
-            
-              articleData.forEach((data) {
-                loadedNews.add(
-                  News(
-                    id:data['id'],
-                    thumbNail: data['fields']['thumbnail'],
-                    title: data['webTitle'],
-                    date: data['webPublicationDate'],
-                    author: data['tags'][0]['webTitle'],
-                    content: data['fields']['body'],
-                    url:data['webUrl'],
-                    user: widget.user.email
-                  ));
-              });
-            _items = loadedNews;
+    try {
+      final response = await http.get(url);
 
-           // print(_items[0].author);
-          }catch(error){
-            //throw(error);
-            print(error);
-          }   
-      }
+      final data = json.decode(utf8.decode(response.bodyBytes));
 
-  @override               
-    void initState(){
-    super.initState();
-      
+      final articleData = data['response']['results'] as List<dynamic>;
+
+      //  List<News> loadedNews = [];
+      var loadedNews = new List<News>();
+      articleData.forEach((data) {
+        print('${data['tags']} abcde');
+        if (data['tags'].length == 0) {
+          print("wtd");
+          return;
+        }
+        loadedNews.add(News(
+            id: data['id'],
+            thumbNail: data['fields']['thumbnail'],
+            title: data['webTitle'],
+            date: data['webPublicationDate'],
+            author: data['tags'][0]['webTitle'],
+            content: data['fields']['body'],
+            url: data['webUrl'],
+            user: widget.user.email));
+      });
+      print("abc");
+      _items = loadedNews;
+    } catch (error) {
+      //throw(error);
+      print(error.toString() + " fetchdataerror");
     }
+  }
 
   @override
-    void didChangeDependencies() {
-      
-      if(_isInit){
-        setState(() {
-          _isLoading = true;
-        });
+  void initState() {
+    super.initState();
+  }
 
-       //1. fetching api for news list
-      fetchNews().then((_){
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      //1. fetching api for news list
+      fetchNews().then((_) {
         setState(() {
-           _isLoading = false;
+          _isLoading = false;
         });
       });
-      }
-
-      _isInit = false;
-      super.didChangeDependencies();
     }
 
+    _isInit = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text('News Feed'),
       ),
       body: RefreshIndicator(
-              onRefresh:() => fetchNews(),
-              child: _isLoading ? Loader() :
-              ListView.builder(
-                itemBuilder: (ctx,index) {
+        onRefresh: () => fetchNews(),
+        child: _isLoading
+            ? Loader()
+            : ListView.builder(
+                itemBuilder: (ctx, index) {
                   return NewsItem(
-                    id:_items[index].id,
-                    thumbNail: _items[index].thumbNail, 
-                    title: _items[index].title, 
-                    date: _items[index].date, 
+                    id: _items[index].id,
+                    thumbNail: _items[index].thumbNail,
+                    title: _items[index].title,
+                    date: _items[index].date,
                     author: _items[index].author,
                     content: _items[index].content,
                     url: _items[index].url,
                     user: _items[index].user,
-                    );
+                  );
                 },
                 itemCount: _items.length,
               ),
       ),
-      );
-      
-      
-    
+    );
   }
 }
